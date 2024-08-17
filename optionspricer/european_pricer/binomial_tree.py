8de1a5fb05b1a1cafb97de466516aec9bb52a6b3
Akira Alexander Wan <akiraalexanderwan@gmail.com>
2024-08-17 14:18:33 +0100

    made BS compatible for dividend payments)

--- optionspricer/european_pricer/binomial_tree.py
+++ optionspricer/european_pricer/binomial_tree.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 
-class BinomialTree:
-    def __init__(self, S0, K, r, T, sigma, steps, q=None, type='call'):
-        self.type = type
+class BinomialTreeEuropean:
+    def __init__(self, S0, K, r, T, sigma, steps, q=None, option_type='call'):
+        self.option_type = option_type
         self.steps = steps
         self.S0 = S0
         self.K = K
@@ -14,11 +14,8 @@ class BinomialTree:
         self.dt = self.T/steps
         self.u = np.exp(self.sigma * np.sqrt(self.dt))
         self.d = 1/self.u
-        if q is not None:
-            self.p = (np.exp((self.r-self.q)*self.dt) - self.d) / (self.u - self.d)
-        else:
-            self.p = (np.exp(self.r*self.dt) - self.d) / (self.u - self.d)
-        
+        self.p = (np.exp((self.r-self.q)*self.dt) - self.d) / (self.u - self.d)
+        self.price = self._price()
 
     def _price(self):
         stock_tree = np.zeros((self.steps+1, self.steps+1))
@@ -28,14 +25,13 @@ class BinomialTree:
         
         option_tree = np.zeros((self.steps+1, self.steps+1))
         for i in range(self.steps+1):
-            option_tree[i, self.steps] = max(0, stock_tree[i, self.steps] - self.K) if (self.type == 'call') else max(0, self.K - stock_tree[i, self.steps])
+            option_tree[i, self.steps] = max(0, stock_tree[i, self.steps] - self.K) if (self.option_type == 'call') else max(0, self.K - stock_tree[i, self.steps])
 
         for j in range(self.steps-1, -1, -1):
             for i in range(j+1):
-                option_tree[i, j] = np.exp(-self.r*self.dt) * (self.p * option_tree[i, j+1] + (1-self.p) * option_tree[i+1, j+1])
+                option_tree[i, j] = np.exp(-(self.r-self.q)*self.dt) * (self.p * option_tree[i, j+1] + (1-self.p) * option_tree[i+1, j+1])
 
         return option_tree[0, 0]
-        
 
     def delta(self):
         pass
--- optionspricer/european_pricer/explicit_formulae.py
+++ optionspricer/european_pricer/explicit_formulae.py
@@ -6,15 +6,67 @@ from scipy.stats import norm
 class BSMFormulae:
     ''' 
     Class to compute the Black-Scholes-Merton formulae for European options.
-    Assumes that interest rates and volatility are constant and stock doesn't pay dividends.
+    Also adjusted for dividend paymenets.
     '''
-    def __init__(self, S0, K, r, T, sigma, type='call'):
+    def __init__(self, S0, K, r, T, sigma, q=0, option_type='call'):
         self.S0 = S0
         self.K = K
         self.r = r
         self.T = float(T)/365
         self.sigma = sigma
-        self.type = type
+        self.q = q
+        self.option_type = option_type
+        self.price = self._price()
+
+    def d1(self):
+        return (np.log(self.S0 / self.K) + (self.r - self.q + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
+    
+    def d2(self):
+        return self.d1() - self.sigma * np.sqrt(self.T)
+    
+    def _price(self):
+        if self.option_type == 'call':
+            return self.S0 * np.exp(-self.q * self.T) * norm.cdf(self.d1()) - self.K * np.exp(-self.r * self.T) * norm.cdf(self.d2())
+        else:
+            return self.K * np.exp(-self.r * self.T) * norm.cdf(-self.d2()) - self.S0 * np.exp(-self.q * self.T) * norm.cdf(-self.d1())
+        
+    def _delta(self):
+        if self.option_type == 'call':
+            return norm.cdf(self.d1())
+        else:
+            return norm.cdf(self.d1()) - 1
+        
+    def _gamma(self):
+        return norm.pdf(self.d1()) / (self.S0 * self.sigma * np.sqrt(self.T))
+
+    def _theta(self):
+        if self.option_type == 'call':
+            return -self.S0 * norm.pdf(self.d1()) * self.sigma / (2 * np.sqrt(self.T)) - self.r * self.K * np.exp(-self.r * self.T) * norm.cdf(self.d2())
+        else:
+            return -self.S0 * norm.pdf(self.d1()) * self.sigma / (2 * np.sqrt(self.T)) + self.r * self.K * np.exp(-self.r * self.T) * norm.cdf(-self.d2())
+        
+    def _vega(self):     
+        return self.S0 * norm.pdf(self.d1()) * np.sqrt(self.T)
+        
+    def _rho(self):
+        if self.option_type == 'call':
+            return self.K * self.T * np.exp(-self.r * self.T) * norm.cdf(self.d2())
+        else:
+            return -self.K * self.T * np.exp(-self.r * self.T) * norm.cdf(-self.d2())
+        
+
+class BSMFormulae:
+    ''' 
+    Class to compute the Black-Scholes-Merton formulae for European options.
+    Assumes that interest rates and volatility are constant and stock doesn't pay dividends.
+    '''
+    def __init__(self, S0, K, r, T, sigma, option_type='call'):
+        self.S0 = S0
+        self.K = K
+        self.r = r
+        self.T = float(T)/365
+        self.sigma = sigma
+        self.option_type = option_type
         self.price = self._price()
 
     def d1(self):
@@ -24,35 +76,8 @@ class BSMFormulae:
         return self.d1() - self.sigma * np.sqrt(self.T)
     
     def _price(self):
-        if self.type == 'call':
+        if self.option_type == 'call':
             return self.S0 * norm.cdf(self.d1()) - self.K * np.exp(-self.r * self.T) * norm.cdf(self.d2())
         else:
             return self.K * np.exp(-self.r * self.T) * norm.cdf(-self.d2()) - self.S0 * norm.cdf(-self.d1())
         
-    def _delta(self):
-        if self.type == 'call':
-            return norm.cdf(self.d1())
-        else:
-            return norm.cdf(self.d1()) - 1
-        
-    def _gamma(self):
-        return norm.pdf(self.d1()) / (self.S0 * self.sigma * np.sqrt(self.T))
-
-    def _theta(self):
-        if self.type == 'call':
-            return -self.S0 * norm.pdf(self.d1()) * self.sigma / (2 * np.sqrt(self.T)) - self.r * self.K * np.exp(-self.r * self.T) * norm.cdf(self.d2())
-        else:
-            return -self.S0 * norm.pdf(self.d1()) * self.sigma / (2 * np.sqrt(self.T)) + self.r * self.K * np.exp(-self.r * self.T) * norm.cdf(-self.d2())
-        
-    def _vega(self):     
-        return self.S0 * norm.pdf(self.d1()) * np.sqrt(self.T)
-        
-    def _rho(self):
-        if self.type == 'call':
-            return self.K * self.T * np.exp(-self.r * self.T) * norm.cdf(self.d2())
-        else:
-            return -self.K * self.T * np.exp(-self.r * self.T) * norm.cdf(-self.d2())
-        
-
-
-    
