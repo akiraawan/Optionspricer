f412068ef21661d667038e1c09dc5cc1779bad7c
James Wu <jameswu144@gmail.com>
2024-09-05 00:12:27 +0100

    minor design change

--- options/binomial_tree/binomial_tree.py
+++ options/binomial_tree/binomial_tree.py
@@ -40,9 +40,9 @@ class BinomialTree:
         self.p = (np.exp((self.r-self.q)*self.dt) - self.d) / (self.u - self.d)
 
         self.generate_trees()
-        self.price = self._price()
+        self.price = self.get_price()
 
-    def _price(self):
+    def get_price(self):
         return self.option_tree[0, 0]
 
     @abstractmethod
