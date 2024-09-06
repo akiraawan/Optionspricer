f2b766669eb6653beaab36c80c553c10f82efd14
James Wu <jameswu144@gmail.com>
2024-09-06 14:49:46 +0100

    small optimisation

--- options/monte_carlo/monte_carlo_european.py
+++ options/monte_carlo/monte_carlo_european.py
@@ -5,15 +5,15 @@ from .monte_carlo import MonteCarlo
 
 class MonteCarloEuropean(MonteCarlo):
     def calculate_price(self):
-        self.stock_prices = np.zeros((self.sims, self.steps+1))  # each row is a path
-        self.stock_prices[:, 0] = self.S0
+        self.stock_prices = np.zeros((self.steps + 1, self.sims))
+        self.stock_prices[0] = self.S0
 
         # Generate stock prices
         for t in range(1, self.steps+1):
             Z = norm.rvs(size=self.sims)
-            self.stock_prices[:, t] = self.stock_prices[:, t-1] * np.exp(((self.r - self.q) - 0.5 * self.sigma**2) * self.dt + self.sigma * np.sqrt(self.dt) * Z)
+            self.stock_prices[t] = self.stock_prices[t-1] * np.exp(((self.r - self.q) - 0.5 * self.sigma**2) * self.dt + self.sigma * np.sqrt(self.dt) * Z)
 
-        self.payoffs = np.maximum(0, self.stock_prices[:, -1] - self.K) if (self.option_type == 'call') else np.maximum(0, self.K - self.stock_prices[:, -1])
+        self.payoffs = np.maximum(0, self.stock_prices[-1] - self.K) if (self.option_type == 'call') else np.maximum(0, self.K - self.stock_prices[-1])
 
         self.discounted_payoffs = np.exp(-self.r * self.T) * self.payoffs
 
