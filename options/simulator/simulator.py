49d6e2be07201e00a47a2c2d53eb88b477b61cd4
Akira Alexander Wan <116723563+akiraawan@users.noreply.github.com>
2024-10-19 17:42:49 +0100

    created skeleton code in simulator folder for yu-en and also created supervised, unsupervised

---
+++ options/simulator/simulator.py
@@ -0,0 +1,18 @@
+
+
+class Simulator:
+    def __init__(self, option_type='call', initial_stock_price_range=(50, 150), strike_price_range=(50, 150), volatility_range=(0.1, 0.5), maturity_range=(0.1, 1), risk_free_rate_range=(0.01, 0.1), dividend_yield_range=(0, 0.1), num_samples=1000):
+        self.option_type = option_type
+        self.initial_stock_price_range = initial_stock_price_range
+        self.strike_price_range = strike_price_range
+        self.volatility_range = volatility_range
+        self.maturity_range = maturity_range
+        self.risk_free_rate_range = risk_free_rate_range
+        self.dividend_yield_range = dividend_yield_range
+        self.num_samples = num_samples
+
+    def simulate(self):
+        raise NotImplementedError("Subclasses should implement this method")
+    
+    def add_label(self):
+        raise NotImplementedError("Subclasses should implement this method")
