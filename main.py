53cf242f31a11fa5e5ae5eeda948549836938f66
akiraawan <akiraalexanderwan@gmail.com>
2024-11-23 16:11:51 +0000

    main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from options import monte_carlo
 from options import neural
 from options.neural.supervised import supervised_european
 from options.neural.unsupervised import unsupervised_european
-import torch
-import matplotlib.pyplot as plt
 
 
 def test_american_pricer():
@@ -66,52 +64,37 @@ def test_monte_carlo():
     print(monte_eur.price)
     print(monte_ame.price)
 
-
 def test_supervised_european_neural_network():
-    option_type = 'call'
-    S = 100.0  # Current price of the underlying asset
-    K = 110.0  # Strike price of the option
-    r = 0.05  # Risk-free interest rate
-    sigma = 0.2  # Volatility of the underlying asset
-    T = 365  # Time to maturity of the option specified in days
-    q = 0.2  # Dividend yield of the underlying asset
-
-    simulator = supervised_european.SimulatorEuropean(option_type=option_type, create_label=True)
+    simulator = supervised_european.SimulatorEuropean(create_label=True)
     model = supervised_european.European_Sup_NN()
     train_loss = supervised_european.train(model, simulator)
-    supnn = model(torch.tensor([[S, K, r, sigma, float(T)/365, q]], dtype=torch.float32))
-
-    print("Supervised Neural Network Price:", supnn.item())
-
-    # Plot the training loss
-    plt.plot(train_loss)
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.title('Training Loss over Epochs')
-    plt.show()
-
+    return model, train_loss
 
 def test_unsupervised_european_neural_network():
-    option_type = 'call'
+    simulator = unsupervised_european.SimulatorEuropean()
+    model = unsupervised_european.European_Unsup_NN()
+    train_loss = unsupervised_european.train(model, simulator)
+    return model, train_loss
+
+def compare(is_european):
     S = 100.0  # Current price of the underlying asset
     K = 110.0  # Strike price of the option
     r = 0.05  # Risk-free interest rate
     sigma = 0.2  # Volatility of the underlying asset
     T = 365  # Time to maturity of the option specified in days
+    steps = 100
+    sims = 100000
     q = 0.2  # Dividend yield of the underlying asset
-    simulator = unsupervised_european.SimulatorEuropean(option_type=option_type)
-    model = unsupervised_european.European_Unsup_NN()
-    train_loss = unsupervised_european.train(model, simulator)
-    unsupnn = model(torch.tensor([[S, K, r, sigma, T, q]], dtype=torch.float32))
 
-    print("Unsupervised Neural Network Price:", unsupnn.item())
+    if is_european:
+        bt = binomial_tree.BinomialTreeEuropean(S, K, r, T, sigma, steps, q, option_type='call')
+        monte = monte_carlo.MonteCarloEuropean(S, K, r, T, sigma, steps, sims, q)
+    else:
+        bt = binomial_tree.BinomialTreeAmerican(S, K, r, T, sigma, steps, q, option_type='call')
+        monte = monte_carlo.MonteCarloAmerican(S, K, r, T, sigma, steps, sims, q)
 
-    # Plot the training loss
-    plt.plot(train_loss)
-    plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.title('Training Loss over Epochs')
-    plt.show()
+    print(bt.price)
+    print(monte.price)
 
 
 def test_simulator():
@@ -120,18 +103,14 @@ def test_simulator():
 
 
 def main():
-    # test_american_pricer()
-    test_european_pricer()
+    test_american_pricer()
+    # test_european_pricer()
     # test_visualise_and_display()
     # test_monte_carlo()
-    test_supervised_european_neural_network()
+    # test_supervised_european_neural_network()
     # model, train_loss = test_unsupervised_european_neural_network()
     
 
 if __name__ == '__main__':
     # compare(is_european=False)
-<<<<<<< HEAD
-    test_american_pricer()
-=======
     main()
->>>>>>> 24b436ec41064c894705002f5c50b7b5e6822789
